3a578828572c3b3b2d1dacc32f054e08f6e08fe8
owengaming001 <42657486+owengaming001@users.noreply.github.com>
2018-11-23 13:06:02 -0330

    Blueberry5
    -Added An Optional Wallpaper
    -Added Built In Discord API

--- BlueberryProject/Install.sh
+++ BlueberryProject/Install.sh
@@ -1,3 +1,4 @@
 sudo cp -r /home/pi/Desktop/BlueberryProject/boot /
 sudo cp -r /home/pi/Desktop/BlueberryProject/raspberrypi-artwork /usr/share/
+sudo cp -r /home/pi/Desktop/BlueberryProject/python3.5 /usr/lib/
 reboot
